6b2647a73d0e36ee4302365161c7115a6b927755
luzangelacarabali <165611205+luzangelacarabali@users.noreply.github.com>
2024-04-19 18:32:07 -0500

    primer docker

---
+++ db/init.sql
@@ -0,0 +1,27 @@
+create database almacen2;
+use almacen almacen2;
+
+create table usuarios(
+    nombre varchar(50),
+    email varchar(50),
+    usuario varchar(50) PRIMARY KEY,
+    password varchar(50)
+);
+
+create table productos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(50),
+    precio decimal(10,2),
+    inventario int
+);
+
+create table ordenes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombreCliente varchar(50),
+    emailCliente varchar(50),
+    totalCuenta decimal(10,2),
+    fecha DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO usuarios (nombre, email, usuario, password) VALUES ('Administrador', 'administrador@almacen2.com', 'admin', '1234');
+INSERT INTO usuarios (nombre, email, usuario, password) VALUES ('hassly', 'hassli@gmail.com', 'hassly', '1234');
---
+++ microservicios/Ordenes/src/models/ordenesModel.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'almacen2',
+    port: 3307,
+
+});
+
+async function crearOrden(orden) {
+    const nombreCliente = orden.nombreCliente;
+    const emailCliente = orden.emailCliente;
+    const totalCuenta = orden.totalCuenta;
+
+    const result = await connection.query('INSERT INTO ordenes VALUES (null, ?, ?, ?, Now())', [nombreCliente, emailCliente, totalCuenta]);
+    return result;
+}
+
+async function traerOrden(id) {
+    const result = await connection.query('SELECT * FROM ordenes WHERE id = ? ', id);
+    return result[0];
+}
+
+async function traerOrdenes() {
+    const result = await connection.query('SELECT * FROM ordenes ');
+    return result[0];
+}
+module.exports = {
+    crearOrden,
+    traerOrden,
+    traerOrdenes
+};
